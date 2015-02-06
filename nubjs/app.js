45c97c025d875e94870110b82025f986fd4f862a
Greg Boyington <greg@automagick.us>
2015-02-06 15:10:36 -0500

    adding hello-world response

--- nubjs/app.js
+++ nubjs/app.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
+
+app.get('/', function(req, res) {
+    res.send('NUBBIN');
+});
+
+
 var server = app.listen(3000, function() {
     console.log('Express is listening to http://localhost:3000'); 
 });
